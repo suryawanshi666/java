10577e5bec2c888b75e3ead133928a9be6770681
Ashwini Suryawanshi <141485991+suryawanshi666@users.noreply.github.com>
2024-08-04 21:19:03 +0530

    Create compareTo.java

---
+++ compareTo.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class file
+{
+    public static void main(String arg[])
+    {
+        String s1="php";
+        String s2="php";
+        int k=s1.compareTo(s2);
+        System.out.println(k);
+    }
+}
